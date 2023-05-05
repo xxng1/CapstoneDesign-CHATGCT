9eee058a7a6579fb28e5f667111eddeee6e18711
xxng1 <woongaa1@naver.com>
2023-05-05 16:40:31 +0900

    Fixes: DB에서 정보를 가져올 수 있게 js파일 및 ejs파일 수정

--- routes/login.js
+++ routes/login.js
@@ -1,5 +1,15 @@
 const express = require('express');
 const router = express.Router();
+var db = require("./db");
+var qs = require("querystring");
+function authIsOwner(request, response) {
+    //console.log(request.session);
+    if (request.session.is_logined) {
+      return true;
+    } else {
+      return false;
+    }
+}
 
 router.get('/', (req, res) => {
     res.render("user/login.ejs", { user: "" })
@@ -15,6 +25,33 @@ router.get('/find_pw/', (req, res) => {
 
 router.get('/join/', (req, res) => {
     res.render("user/join.ejs", { user: "" })
+});
+//추가
+router.post('/login_process', (request, response) => {
+    var body = "";
+        request.on("data", function (data) {
+          body = body + data;
+        });
+        request.on("end", function () {
+          var post = qs.parse(body);
+          db.query(
+            `SELECT loginid, password, class FROM person WHERE loginid = ? and password = ?`,
+            [post.id, post.pw],
+            function (error, result) {
+              if (error) {
+                throw error;
+              }
+              if (result[0] === undefined) response.end("Who ?");
+              else {
+                request.session.is_logined = true;
+                request.session.login_id = result[0].loginid;
+                request.session.class = result[0].class;
+                
+                response.redirect("/");
+                response.end('Welcome !!!');
+              }
+            });
+        });
 });
 
 module.exports = router;
--- routes/mypage.js
+++ routes/mypage.js
@@ -1,9 +1,38 @@
 const express = require('express');
 const router = express.Router();
+var db = require("./db");
+var qs = require("querystring");
+function authIsOwner(request, response) {
+  if (request.session.is_logined) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
+router.get('/', (req, res) => {
+    db.query(`SELECT * FROM person `, function (error, result) {
+        if (error) {
+          throw error;
+        }
+        tmplogin = "Y";
+        var context = {
+          loggined: authIsOwner(req, res),
+          id: req.session.login_id,
+          cls: req.session.class,
+          results: result,
+        };
+        req.app.render("user/mypage.ejs", context, function (err, html) {
+        res.end(html);
+        });
+      });
+});
+
+/**
 router.get('/', (req, res) => {
     res.render("user/mypage.ejs", { user: "" })
 });
+ */
 
 router.get('/change_id/', (req, res) => {
     res.render("user/change_id.ejs", { user: "" })
