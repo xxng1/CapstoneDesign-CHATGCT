b9658c7243cba95153d83eb0d021182d6370e863
박상웅 <woongaa1@naver.com>
2023-05-31 11:19:12 +0900

    Feat: 관리자페이지/ 유저 학과 통계 기능 추가

--- routes/user_manage.js
+++ routes/user_manage.js
@@ -12,6 +12,24 @@ function authIsOwner(request, response) {
   }
 }
 
+//--
+function calculateSubjectCount(subject, callback) {
+  db.query(
+    `SELECT COUNT(*) AS count FROM user WHERE subject = ?`,
+    [subject],
+    function (error, result) {
+      if (error) {
+        callback(error, null);
+      } else {
+        // Extract the count from the result
+        const count = result[0].count;
+        callback(null, count);
+      }
+    }
+  );
+}
+//--
+
 router.get("/", (request, response) => {
   db.query(`SELECT * FROM user `, function (error, result) {
     if (error) {
@@ -46,6 +64,7 @@ router.get("/update/:userId", (request, response) => {
         loginid: result[0].loginid,
         password: result[0].password,
         name:  result[0].name,
+        studentnum:  result[0].studentnum,
         class:  result[0].class,
 
         pId: userId,
@@ -69,8 +88,8 @@ router.post("/update_process/:userId", (request, response) => {
     var users = qs.parse(body);
     userId = request.params.userId;
     db.query(
-      "UPDATE user SET loginid=?, password=?, name=?, class=? WHERE id=?",
-      [users.loginid, users.password, users.name, users.class, userId],
+      "UPDATE user SET loginid=?, password=?, name=?, studentnum=?, class=? WHERE id=?",
+      [users.loginid, users.password, users.name, users.studentnum, users.class, userId],
       function (error, result) {
         response.writeHead(302, { Location: `/user_manage` });
         response.end();
@@ -93,6 +112,84 @@ router.get("/delete_process/:userId", (request, response) => {
     }
   );
 });
+
+
+// router.get("/user_stats", (request, response) => {
+//   db.query(`SELECT * FROM user WHERE subject IN (
+//       SELECT subject FROM user GROUP BY subject
+//       HAVING COUNT(*) >= 3)
+//    `, function (error, result) {
+//     if (error) {
+//       throw error;
+//     }
+//     var context = {
+//       doc: `user/user_stats.ejs`,
+//       loggined: authIsOwner(request, response),
+//       id: request.session.login_id,
+//       cls: request.session.class,
+//       results: result,
+//     };
+//     request.app.render("user/user_stats.ejs", context, function (err, html) {
+//       response.end(html);
+//     });
+//   });
+// });
+
+//학과
+router.get("/user_stats", (request, response) => {
+  db.query(
+    `SELECT subject, COUNT(*) AS count FROM user GROUP BY subject ORDER BY count DESC`,
+    
+    function (error, result) {
+      if (error) {
+        throw error;
+      }
+      var context = {
+        doc: `user/user_stats.ejs`,
+        //counts: result[0].count,
+        loggined: authIsOwner(request, response),
+        id: request.session.login_id,
+        cls: request.session.class,
+        results: result,
+        
+      };
+      request.app.render("user/user_stats.ejs", context, function (err, html) {
+        if (err) {
+          throw err;
+        }
+        response.end(html);
+      });
+    }
+  );
+});
+
+//학번
+
+// router.get("/user_stats_num", (request, response) => {
+//   db.query(
+//     `SELECT subject, COUNT(*) AS count FROM user GROUP BY subject ORDER BY count DESC`,
+//     function (error, result) {
+//       if (error) {
+//         throw error;
+//       }
+//       var context = {
+//         doc: `user/user_stats.ejs`,
+//         //counts: result[0].count,
+//         loggined: authIsOwner(request, response),
+//         id: request.session.login_id,
+//         cls: request.session.class,
+//         results: result,
+        
+//       };
+//       request.app.render("user/user_stats.ejs", context, function (err, html) {
+//         if (err) {
+//           throw err;
+//         }
+//         response.end(html);
+//       });
+//     }
+//   );
+// });
 
 
 
