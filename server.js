778671c0c095f8399956015429b91861c622dafc
박상웅 <woongaa1@naver.com>
2023-06-02 13:53:18 +0900

    Design: 그래프 크기 축소

--- server.js
+++ server.js
@@ -179,7 +179,7 @@ function runTokenizerScript(msg) {
   });
 }
 
-const PORT = process.env.PORT || 60009;
+const PORT = process.env.PORT || 60010;
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
